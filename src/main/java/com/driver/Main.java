4e3b3a01eba28747abcd79943c3b07cb000869d5
wtricks <webtricks.ak@gmail.com>
2023-04-24 12:49:22 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println(p.product(5, 5));
+
+        System.out.println(p.product(5, 5, 5));
+
+        System.out.println(p.product(0.5, 0.5));
+    }
 }
